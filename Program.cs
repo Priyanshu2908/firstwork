1b1b0e0d2e527a5fd48412d01407446b967cf7a9
Priyanshu2908 <prianshuchaubey29@gmail.com>
2024-02-05 11:30:30 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Net;
+
+public class Program
+{
+    public static void Main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            Console.Write("Enter word " + (i + 1) + ": ");
+            string word = Console.ReadLine();
+
+            string query = "https://www.google.com/search?q=define+" + WebUtility.UrlEncode(word);
+            Process.Start(new ProcessStartInfo("cmd", $"/c start {query}") { CreateNoWindow = true });
+        }
+    }
+}
